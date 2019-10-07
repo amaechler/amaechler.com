5250ec4541917738eb759515eb95e605ebda688a
Andreas Maechler <amaechler@gmail.com>
2019-10-07 08:42:57 -0600

    Move blog to root

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,14 @@ module.exports = {
     title: `extralife`,
     author: `Andreas Maechler`,
     social: [
-    ]
-  }
+      {
+        name: `linkedin`,
+        url: `https://www.linkedin.com/in/andreasmaechler/`,
+      },
+      {
+        name: `github`,
+        url: `https://github.com/amaechler`,
+      },
+    ],
+  },
 }
---
+++ public/styles-36873fdf6b9a614304e5.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{204:function(n,o,w){},205:function(n,o,w){}}]);
+//# sourceMappingURL=styles-36873fdf6b9a614304e5.js.map

846e8ee625cacb042d8e023ee0f323e8be46d368
Andreas Maechler <amaechler@gmail.com>
2019-10-07 08:22:20 -0600

    Initial version with rstudio post

---
+++ blog/src/gatsby-theme-blog/components/bio-content.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react"
+import { Styled } from "theme-ui"
+
+/**
+ * Shadow me to add your own bio content.
+ */
+
+export default () => (
+  <Fragment>
+    Words by <Styled.a href="https://amaechler.com/">Andreas Maechler</Styled.a>.
+    <br />
+    Change me. This is all quite default.
+  </Fragment>
+)

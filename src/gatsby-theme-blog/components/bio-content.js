a4df824e8fb7467ad4110f87bc759c6ee971aab7
Andreas Maechler <amaechler@gmail.com>
2020-08-23 09:56:28 -0600

    Bump dependencies

--- src/gatsby-theme-blog/components/bio-content.js
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -1,14 +1,16 @@
-import React, { Fragment } from "react"
+import React from "react"
 import { Styled } from "theme-ui"
 
 /**
- * Shadow me to add your own bio content.
+ * Change the content to add your own bio
  */
 
-export default () => (
-  <Fragment>
+export default function Bio() {
+  return (
+    <>
     Words by <Styled.a href="https://amaechler.com/">Andreas Maechler</Styled.a>.
     <br />
     Change me. This is all quite default.
-  </Fragment>
-)
+    </>
+  )
+}

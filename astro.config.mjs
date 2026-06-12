bba300d7b5268def03c1c1cd10940ddac4ebad48
Andreas Maechler <amaechler@gmail.com>
2026-06-12 16:24:57 -0600

    Use the one-light Shiki theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
     site: site.siteUrl,
     integrations: [sitemap()],
     markdown: {
-        shikiConfig: { theme: "github-light" },
+        shikiConfig: { theme: "one-light" },
         remarkPlugins: [smartypants]
     }
 });
